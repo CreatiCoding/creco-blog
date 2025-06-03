e5b4e5e3d8aae2cdf88247abdd2f97a7baa639a5
정석호 <creaticoding@gmail.com>
2025-06-03 15:56:37 +0900

    deploy post-test

--- services/blog.creco.dev-post/next.config.ts
+++ services/blog.creco.dev-post/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: process.env.LOCAL === 'true' ? undefined : "export",
-  basePath: "/post",
+  basePath: "/post-test",
 
   rewrites: process.env.LOCAL === 'true' ? async () => {
     return [
