9c4d5562cb40ab46919ebbe29a976ff976fda36d
정석호 <creaticoding@gmail.com>
2025-06-03 19:51:28 +0900

    upadte posts metatag

--- services/blog.creco.dev-post/src/app/page.tsx
+++ services/blog.creco.dev-post/src/app/page.tsx
@@ -1,3 +1,14 @@
+import { Metadata } from "next";
 import { PostsPage } from "./PostsPage";
+
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: 'Creco&apos;s Blog',
+        description: 'developer for developer',
+        openGraph: {
+            images: ['https://divopsor.github.io/blog-images/2025/creco-s-blog.png']
+        },
+    }
+}
 
 export default PostsPage;
